be9dc0d68cd17fbb8cf83039b4579d8541795ec3
Kidy Lee <kidylee@gmail.com>
2017-11-01 19:39:09 +0000

    init account  controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
  
+ 
+   get 'account',  to: 'account#show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
